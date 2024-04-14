44c3f7452abeb08664df1072da58cd282557d788
taehee <eikz.il2ljlpl@gmail.com>
2024-04-15 04:35:52 +0900

    Remove unnecessary parts

--- DataFormats/src/Gen.cc
+++ DataFormats/src/Gen.cc
@@ -4,13 +4,11 @@ ClassImp(Gen)
 
 Gen::Gen() : Particle() {
 
-    j_IsEmpty = true; //?
+    j_IsEmpty = true;
     j_index = -1;
     j_PID = 0;
     j_status = 0;
     j_mother_index = -1;
-    /*j_isPrompt = false;
-     */
     j_status_flags = -1;
     
 }
@@ -37,23 +35,3 @@ void Gen::SetGenStatusFlags(unsigned short f){
 void Gen::Print() const{
     cout << "(Index, PID, Status, MotherIndex, Pt, Eta, Phi, M) = " << Index() << "\t" << PID() << "\t" << Status() << "\t" << MotherIndex() << "\t" << Pt() << "\t" << Eta() << "\t" << Phi() << "\t" << M() << endl;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
