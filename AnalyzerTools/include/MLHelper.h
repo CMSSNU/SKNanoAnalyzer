c3dcc291e3c291152a857da2dde4a736da455fd6
kyj519 <kyj519@snu.ac.kr>
2024-12-27 01:10:30 +0900

    MLHelper and SystematicHelper

--- AnalyzerTools/include/MLHelper.h
+++ AnalyzerTools/include/MLHelper.h
@@ -1,44 +1,57 @@
+// MLHelper.h
 #ifndef MLHELPER_H
 #define MLHELPER_H
 
-#ifdef ClassDef
-#undef ClassDef
-#endif
-#include <torch/script.h> // PyTorch header
-#include <unordered_map>
+#include <string>
 #include <vector>
-#include "onnxruntime_cxx_api.h"
+#include <memory>
+#include <unordered_map>
+#include <variant>
+// Forward declaration of the implementation class
+class MLHelperImpl;
 
+// Type aliases for clarity
+using FloatArray = std::vector<float>;
+using FloatArrays = std::vector<FloatArray>;
+using IntArray = std::vector<int>;
+using BoolArray = std::vector<uint8_t>;
+using VariousArray = std::variant<FloatArray, IntArray, BoolArray>;
 
-class MLHelper{
-    public:
-        enum class ModelType
-        {
-            ONNX,
-            TORCHSCRIPT
-        };
-
-        MLHelper(std::string modelPath, ModelType modelType);
-        ~MLHelper();
-        void Load_TorchScript_Model(std::string modelPath);
-        void Load_ONNX_Model(std::string modelPath);
-
-        std::string modelPath;
-        ModelType modelType;
-
-
-
-    private:
-        // ONNX
-        Ort::Env env;
-        const std::shared_ptr<Ort::Session> session;
-        const std::vector<std::string> inputNodeNames;
-        const std::vector<std::string> outputNodeNames;
-        const std::unordered_map<std::string, std::vector<int>> inputShapes;
-        const std::unordered_map<std::string, std::vector<int>> outputShapes;
-        // TorchScript
-        torch::jit::script::Module module;
+class MLHelper
+{
+public:
+    // Enumeration for model types
+    enum class ModelType
+    {
+        ONNX,
+        TORCHSCRIPT
     };
 
+    // Constructor and Destructor
+    MLHelper(const std::string &modelPath, ModelType modelType);
+    ~MLHelper();
 
-#endif
+    // Delete copy constructor and copy assignment operator
+    MLHelper(const MLHelper &) = delete;
+    MLHelper &operator=(const MLHelper &) = delete;
+
+    // Implement move semantics
+    MLHelper(MLHelper &&) noexcept;
+    MLHelper &operator=(MLHelper &&) noexcept;
+
+    // Load model methods
+    // void Load_TorchScript_Model(const std::string &modelPath);
+    void Load_ONNX_Model(const std::string &modelPath);
+    // Run model methods
+    std::unordered_map<std::string, FloatArray> Run_ONNX_Model(const std::unordered_map<std::string, VariousArray> &inputDataMap, const std::unordered_map<std::string, IntArray> &inputDataShapeMap);
+    // FloatArrays Run_TorchScript_Model(const FloatArray &inputData);
+
+    // Getter for the model type
+    ModelType GetModelType() const;
+
+private:
+    // Pointer to the implementation
+    std::unique_ptr<MLHelperImpl> pImpl;
+};
+
+#endif // MLHELPER_H
