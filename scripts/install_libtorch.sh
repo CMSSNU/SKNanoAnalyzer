b43052d217627515a2c49ac35fed1b948fd0e918
Jin Choi <choij@cern.ch>
2025-01-30 13:44:28 +0900

    Update install_libtorch.sh

--- scripts/install_libtorch.sh
+++ scripts/install_libtorch.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 cd $SKNANO_HOME/external
 #print in yellow
 echo -e "\e[93m@@@@ Downloading LibTorch\e[0m"
-wget https://download.pytorch.org/libtorch/nightly/cpu/libtorch-shared-with-deps-latest.zip -O libtorch.zip
+wget --no-verbose https://download.pytorch.org/libtorch/nightly/cpu/libtorch-shared-with-deps-latest.zip -O libtorch.zip
 unzip -q libtorch.zip -d libtorch
 cd libtorch
 mv libtorch/* ./
